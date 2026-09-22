433b8751f816a4e918b20eef31a6a399a9f2e8c6
Fer <fervazdiaz0606@gmail.com>
2026-09-22 09:44:17 -0600

    💛 s01t0401 for

---
+++ s02/s01t0401_for.py
@@ -0,0 +1,22 @@
+"""
+escribir un programa que calcule
+ la suma de los "n" numeros naturales.
+Por ejemplo si n = 100, el programa
+calculara la suma del 1 al 100
+42
+"""
+# importamos biblioteca time
+import time
+
+#creando una marca de tiempo
+timestamp_01 = time.time()
+
+#programa que calcula las sumas
+# de los "n" numeros naturales
+n = 100
+sum = 0
+
+#ciclo for
+for number in range(1,n+1):
+    sum = sum + n
+print("") 
