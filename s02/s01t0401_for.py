4b43a5e1bcb6061d99b0d7d4f3da71a70c14dfa2
Fer <fervazdiaz0606@gmail.com>
2026-09-22 11:09:06 -0600

    ⭐s01t0401 for

--- s02/s01t0401_for.py
+++ s02/s01t0401_for.py
@@ -14,6 +14,23 @@ timestamp_01 = time.time()
 #programa que calcula las sumas
 # de los "n" numeros naturales
 n = 100
-sum = 0
-  for number in range(1,n+1):
-    print(str)(number)(+ " ")
+total_sum = 0
+
+#ciclo for
+for number in range(1,n+1):
+
+    total_sum = total_sum + number
+    #1: sum<- 0 + 1
+    # sum = 1
+    #2: sum<- 1 + 2
+    # sum = 3
+    #3: sum<- 3 + 3
+    #...
+    #100: sum <- sum_(-1) + 100
+print(f"La suma de 1 hasta {100} es: {total_sum}")
+
+#tomando el tiempo final
+timestamp_02 = time.time()
+
+#impresion del tiempo de ejecucion
+print(f"Tiempo de ejecucion: {(timestamp_02-timestamp_01) * 1e6:.2f} μs")
